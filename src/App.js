04aa295cc8eaf1394012fc643a332b32b063d4cb
alex chudsky <bowlder05@gmail.com>
2021-03-14 19:16:49 +0200

    sub category update

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import AdminRoute from "./components/routes/AdminRoute";
 import CategoryCreate from "./pages/admin/category/CategoryCreate";
 import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
 import SubCreate from "./pages/admin/sub/SubCreate";
+import SubUpdate from "./pages/admin/sub/SubUpdate";
 
 const App = () => {
     const dispatch = useDispatch();
@@ -75,6 +76,7 @@ const App = () => {
                 <AdminRoute exact path='/admin/category/:slug' component={CategoryUpdate}/>
 
                 <AdminRoute exact path='/admin/sub' component={SubCreate}/>
+                <AdminRoute exact path='/admin/sub/:slug' component={SubUpdate}/>
             </Switch>
         </>
     );
