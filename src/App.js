3ee78591b9e0be8d971addc324cb124da681ac3c
alex chudsky <bowlder05@gmail.com>
2021-03-14 15:22:06 +0200

    sub categories functions

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminRoute from "./components/routes/AdminRoute";
 import CategoryCreate from "./pages/admin/category/CategoryCreate";
 import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
+import SubCreate from "./pages/admin/sub/SubCreate";
 
 const App = () => {
     const dispatch = useDispatch();
@@ -64,12 +65,16 @@ const App = () => {
                 <Route exact path='/register' component={Register}/>
                 <Route exact path='/register/complete' component={RegisterComplete}/>
                 <Route exact path='/forgot/password' component={ForgotPassword}/>
+
                 <UserRoute exact path='/user/history' component={History}/>
                 <UserRoute exact path='/user/password' component={Password}/>
                 <UserRoute exact path='/user/wishlist' component={Wishlist}/>
+
                 <AdminRoute exact path='/admin/dashboard' component={AdminDashboard}/>
                 <AdminRoute exact path='/admin/category' component={CategoryCreate}/>
                 <AdminRoute exact path='/admin/category/:slug' component={CategoryUpdate}/>
+
+                <AdminRoute exact path='/admin/sub' component={SubCreate}/>
             </Switch>
         </>
     );
