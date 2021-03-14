55e0208dfe08d4080f6ad40ed300c884bf1f5baa
alex chudsky <bowlder05@gmail.com>
2021-03-14 23:35:45 +0200

    product create form

--- src/pages/product/ProductCreate.js
+++ src/pages/product/ProductCreate.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AdminNav from "../../components/nav/AdminNav";
 
+const initialState = {
+    title: '',
+    description: '',
+    categories: [],
+    category: '',
+    price: '',
+    subs: [],
+    shipping: '',
+    quantity: '',
+    images: [],
+    colors: ['Black', 'White', 'Brown', 'Silver', 'Blue'],
+    brands: ['Apple', 'Microsoft', 'Lenovo', 'Samsung', 'ASUS'],
+    color: '',
+    brand: '',
+};
+
 const ProductCreate = () => {
+    const [values, setValues] = useState(initialState);
+
+    const {
+        title,
+        description,
+        price,
+        categories,
+        category,
+        subs,
+        shipping,
+        quantity,
+        images,
+        colors,
+        brands,
+        color,
+        brand
+    } = values;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleChange = () => {
+
+    };
+
     return (
         <div className="container-fluid">
             <div className="row">
@@ -9,7 +51,102 @@ const ProductCreate = () => {
                     <AdminNav/>
                 </div>
                 <div className="col-md-10">
-                    Product create form
+                    <h4>Product create</h4>
+                    <br/>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label>Title</label>
+                            <input
+                                type='text'
+                                name='title'
+                                className='form-control'
+                                value={title}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Description</label>
+                            <input
+                                type='text'
+                                name='description'
+                                className='form-control'
+                                value={description}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Price</label>
+                            <input
+                                type='number'
+                                name='price'
+                                className='form-control'
+                                value={price}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Shipping</label>
+                            <select
+                                name="shipping"
+                                className="form-control"
+                                onChange={handleChange}
+                            >
+                                <option>Please select</option>
+                                <option value="No">No</option>
+                                <option value="Yes">Yes</option>
+                            </select>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Quantity</label>
+                            <input
+                                type='number'
+                                name='quantity'
+                                className='form-control'
+                                value={quantity}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Color</label>
+                            <select
+                                name="color"
+                                className="form-control"
+                                onChange={handleChange}
+                            >
+                                <option>Please select</option>
+                                {colors.map(c => (
+                                    <option key={c} value={c}>
+                                        {c}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Brand</label>
+                            <select
+                                name="brand"
+                                className="form-control"
+                                onChange={handleChange}
+                            >
+                                <option>Please select</option>
+                                {brands.map(b => (
+                                    <option key={b} value={b}>
+                                        {b}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <br />
+
+                        <button className="btn btn-outline-info">Save</button>
+                    </form>
                 </div>
             </div>
         </div>
