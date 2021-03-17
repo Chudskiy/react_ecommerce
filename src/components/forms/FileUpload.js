97f670bc0f6527263bc3a3dc09e0deeeb7436d3f
alex chudsky <bowlder05@gmail.com>
2021-03-17 21:28:17 +0200

    file resize in React

--- src/components/forms/FileUpload.js
+++ src/components/forms/FileUpload.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import Resizer from 'react-image-file-resizer';
+
 
 const FileUpload = () => {
+    const {user} = useSelector(state => ({...state}));
+
     const fileUploadAndResize = (e) => {
         // console.log(e.target.files);
         // resize
+        const files = e.target.files;
+
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                Resizer.imageFileResizer(
+                    files[i],
+                    720,
+                    720,
+                    'JPEG',
+                    100,
+                    0,
+                    (uri) => {
+                        console.log('URI = ', uri);
+                    },
+                    'base64',
+                )
+            }
+        }
+
         // send back to server to upload to cloudinary
         // set url to images[] in the parent component - ProductCreate
     }
