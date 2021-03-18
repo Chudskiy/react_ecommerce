bd7f0cd5da3c2626a30a6c8f851ea184040fd69c
alex chudsky <bowlder05@gmail.com>
2021-03-18 12:24:59 +0200

    fetch all products in admin dashboard

--- src/functions/product.js
+++ src/functions/product.js
@@ -8,27 +8,7 @@ export const createProduct = async (product, authtoken) =>
         }
     );
 
-
-// export const getProducts = async () =>
-//     await axios.get(`${process.env.REACT_APP_API}/products`);
-//
-// export const getProduct = async (slug) =>
-//     await axios.get(`${process.env.REACT_APP_API}/product/${slug}`);
-//
-// export const removeProduct = async (slug, authtoken) =>
-//     await axios.delete(`${process.env.REACT_APP_API}/product/${slug}`, {
-//         headers: {
-//             authtoken,
-//         }
-//     });
-//
-// export const updateCategory = async (slug, category, authtoken) =>
-//     await axios.put(`${process.env.REACT_APP_API}/product/${slug}`, category,
-//         {
-//             headers: {
-//                 authtoken,
-//             }
-//         }
-//     );
+export const getProductsByCount = async (count) =>
+    await axios.get(`${process.env.REACT_APP_API}/products/${count}`);
 
 
